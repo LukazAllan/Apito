636b7bbad57aa9572cd35180a8b428fe5d2728a4
Allan Lucas <38227482+LukazAllan@users.noreply.github.com>
2021-09-28 02:24:58 +0000

    +new files +audio level correct

--- script.js
+++ script.js
@@ -11,9 +11,29 @@ function drop(ev) {
   var data = ev.dataTransfer.getData("text");
   ev.target.appendChild(document.getElementById(data));
 }
-let audios = {"Alvorada":"alvorada.wav","Bandeira Nacional":"bandeira_nacional.wav","Banho de Água Doce":"Banho_de_agua_doce.wav","Cabo de dia":"Cabo de dia.wav","Imediato":"imediato.wav","Inspeção":"inspecao.wav","Licenciados Formar":"licenciados_formar.wav","Ronda":"mila-ronda.wav","Oficial Intermediário Comandante no próprio":"oficial_intermediário_comandante_no_proprio.m4a","Oficial Superior Comandante no próprio":"oficial_superior_comandante_no_proprio.m4a","Oficial Superior Indo a bordo":"oficial_superior_indo_a_bordo.m4a","Polícia":"Policia.wav","Rancho para Serviço":"rancho_servico.m4a","Render Serviço":"render_servico.wav","Reunir Geral":"reunir_geral.m4a","Silêncio":"silencio.wav","Sinaleiro":"sinaleiro.wav","Uniforme":"Uniforme.wav"};
+let audios = {
+  "Alvorada": "alvorada.mp3",
+  "Bandeira Nacional": "bandeira_nacional.mp3",
+  "Banho de Água Doce": "banho_de_agua_doce.mp3",
+  "Cabo de dia": "Cabo de dia.mp3",
+  "Imediato": "imediato.mp3",
+  "Licenciados Formar": "licenciados_formar.mp3",
+  "Ronda": "mila-ronda.mp3",
+  "Inspeção": "inspecao.mp3",
+  "Oficial Intermediário Comandante no próprio": "oficial_intermediario_comandante_no_proprio.mp3",
+  "Polícia": "Policia.mp3",
+  "Rancho para Serviço": "rancho_servico.mp3",
+  "Render Serviço": "render_servico.mp3",
+  "Reunir Geral": "reunir_geral.mp3",
+  "Silêncio": "silencio.mp3",
+  "Oficial Superior Comandante no próprio": "oficial_superior_comandante_no_proprio.mp3",
+  "Sinaleiro": "sinaleiro.mp3",
+  "Uniforme": "Uniforme.mp3",
+  "Oficial Superior Indo a bordo": "oficial_superior_indo_a_bordo.mp3"
+};
 
 let all_answers = Object.keys(audios);
+console.log(all_answers);
 let respostas = [
 "Inspeção", "Licenciados Formar", "Cabo de Dia", "Reunir Geral", "Sinaleiro"
 ];
@@ -48,35 +68,32 @@ function answer() {
 
 
 function next() {
+  console.log(`todas as respostas: ${all_answers.length}`);
+  respostas_possiveis = [...all_answers];
+  resp_correta = Math.floor(Math.random()*5); // Índice da Resposta 
+  index_resposta = Math.floor(Math.random()*all_answers.length)
+  text_correta = respostas_possiveis[index_resposta]; // Texto ta resposta
   respostas = [];
-  resp_correta = Math.floor(Math.random()*5);
-  text_correta = all_answers[Math.floor(Math.random()*all_answers.length)];
-  
-  for (i = 0;respostas.length < 5; i++){
-    outro = all_answers[Math.floor(Math.random()*all_answers.length)];
+  //console.log(respostas_possiveis)
+  respostas_possiveis.pop(index_resposta);
+  for (i = 0; i < 5; i++){
+    console.log(respostas_possiveis.length)
     if (i == resp_correta){
       respostas.push(text_correta);
     } else {
-      if (outro != resp_correta){
-        Add=true;
-        for (let c = 0; c < respostas.length; c++) {
-          if (respostas[c] == outro){
-            Add=false;
-            break;
-          }
-        }
-        if (Add == true){
-          respostas.push(outro)
-        }
-      }else{pass;}
+      indice = Math.floor(Math.random()*respostas_possiveis.length);
+      respostas.push(respostas_possiveis[indice]);
+      console.log(respostas_possiveis[indice])
+      respostas_possiveis.pop(indice);
     }
   }
- 
+  console.log('-----------------------------------');
+  console.log(respostas_possiveis);
   document.getElementById("respostas").innerHTML = "";
   
   for (i = 0; i < respostas.length; i++){
     document.getElementById("respostas").innerHTML = document.getElementById("respostas").innerHTML + `<span id="resp${i}" class="badge rounded-pill bg-secondary" onclick="answer()"><h2>${respostas[i]}</h2></span>`;
-}
+  }
   play();
   
 }
